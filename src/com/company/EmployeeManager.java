5b02fe7f4b1a3b2a2c7ad14f8fce8e973ca266f2
MuhammetYlmz1 <muhammetylmz278@gmail.com>
2021-05-22 17:09:43 +0300

    commit

---
+++ src/com/company/EmployeeManager.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class EmployeeManager extends PersonManager {
+
+    public  void Employee(){
+        System.out.println("Ayın elemanı getirildi");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Customer customer=new Customer();
+	Employee employee=new Employee();
+    CustomerManager customerManager=new CustomerManager();
+    customerManager.Add();
+    EmployeeManager employeeManager=new EmployeeManager();
+    employeeManager.Employee();
+    }
+}
